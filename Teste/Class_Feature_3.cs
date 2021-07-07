0966e1dc206f7db3fc41f9bced1ba674289c528f
Paulo Santos | Casa <phs.developer@gmail.com>
2021-07-07 05:41:28 -0300

    main: Classe adicionada para gerar um conflito

---
+++ Teste/Class_Feature_3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    class Class_Feature_3
+    {
+        //Classe criada para gerar um conflito
+    }
+}
