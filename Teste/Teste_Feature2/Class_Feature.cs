848a0bdea72dcac000f29f6f6b9b5be46a08b077
Paulo Santos | Casa <phs.developer@gmail.com>
2021-07-06 22:26:14 -0300

    Adicionado um diretório e uma classe a branch feature2

---
+++ Teste/Teste_Feature2/Class_Feature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste.Teste_Feature2
+{
+    class Class_Feature
+    {
+    }
+}
