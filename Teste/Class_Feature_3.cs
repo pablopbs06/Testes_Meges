4bce4029687f2632f4acf2ccbacb3223c55504b8
Paulo Santos | Casa <phs.developer@gmail.com>
2021-07-07 06:08:46 -0300

    resolvendo conflitos no Visual studio

--- Teste/Class_Feature_3.cs
+++ Teste/Class_Feature_3.cs
@@ -4,17 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-<<<<<<< HEAD
-namespace Teste    public class Class_Feature_3
-    {
-        //Criado para gerar um conflito no git
 
-=======
 namespace Teste
 {
     public class Class_Feature_3
     {
+    //Criado para gerar um conflito no git
         //Criado para gerar um conflito no git
->>>>>>> feature3
     }
 }
