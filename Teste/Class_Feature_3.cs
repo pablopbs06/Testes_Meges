74cb0861fbee0e2cfb9dd20e8c4b01aa57128148
Paulo Santos | Casa <phs.developer@gmail.com>
2021-07-07 05:32:04 -0300

    feature3 : Classe criada para gerar um conflito

---
+++ Teste/Class_Feature_3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    public class Class_Feature_3
+    {
+        //Criado para gerar um conflito no git
+    }
+}
