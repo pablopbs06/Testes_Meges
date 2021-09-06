7c03080aa9097cf8e92436c31a49a2895506afba
Pablo santos <pablo.pbs06@hotmail.com>
2021-09-06 11:11:22 -0300

    Metodo teste para conflitos

---
+++ Teste/Classe_20210906.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    public class Classe_20210906
+    {
+        public void MTD_01() => Console.WriteLine("metodo teste 01");
+
+        public void MTD_02()
+        {
+            Console.WriteLine("Metodo teste 02 - mensagem 01");
+            Console.WriteLine("Metodo teste 02 - mensagem 02");
+        }
+    }
+}
